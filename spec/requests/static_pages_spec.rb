20a5590dd2c96f7b1c1a35eec761d906ad1e48db
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-29 12:32:01 -0400

    Add static_pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home PAge" do
+  describe "Home Page" do
     it "should have the content 'Fwitter" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
