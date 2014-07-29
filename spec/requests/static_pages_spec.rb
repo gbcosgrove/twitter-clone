5fc0005464610e3c892f8a61396d7ebaf0888aca
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-29 12:43:50 -0400

    Add About page tests, routes, and views

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,4 +8,20 @@ describe "StaticPages" do
       expect(page).to have_content('Fwitter')
     end
   end
+
+  describe "Help Page" do
+
+    it "Should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+      expect(page).to have_content('Fake Twitter')
+    end
+  end
+
+  describe "About Page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
 end
