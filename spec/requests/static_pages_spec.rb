9db38c9a5327f0f656b9943c4fc98c24d61c693d
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-30 18:31:59 -0400

    Modify contact page to use have_selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,7 +30,7 @@ subject { page }
   describe "Contact Page" do
     before { visit contact_path }
 
-    it { should have_content('Contact Us') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should_not have_title('| Contact') }
   end
 
