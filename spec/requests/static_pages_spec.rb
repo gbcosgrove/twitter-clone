44d3c23dc3b70128e8d904682dedd9d1ba96ca64
Greg Cosgrove <gregcosgrove@gmail.com>
2014-07-30 18:18:52 -0400

    Refactor routes and tests with paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,77 +2,36 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Fwitter - The Fake Twitter" }
+subject { page }
 
   describe "Home Page" do
-    it "should have the content 'Fwitter" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      expect(page).to have_content('Fwitter')
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Fwitter - The Fake Twitter")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Fwitter') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help Page" do
 
-    it "Should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-      expect(page).to have_content('Fake Twitter')
-    end
+    before { visit help_path }
 
-     it "should have the base title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Fwitter - The Fake Twitter")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/help'
-      expect(page).not_to have_title('| Help')
-    end
+    it { should have_content('Help') }
+    it { should_not have_title('| Help') }
   end
 
   describe "About Page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-     it "should have the base title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Fwitter - The Fake Twitter")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/about'
-      expect(page).not_to have_title('| About')
-    end
+    it { should have_content('About Us') }
+    it { should_not have_title('| About') }
   end
 
   describe "Contact Page" do
-    it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact Us')
-    end
+    before { visit contact_path }
 
-      it "should have the base title" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("Fwitter - The Fake Twitter")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/contact'
-      expect(page).not_to have_title('| Contact')
-    end
+    it { should have_content('Contact Us') }
+    it { should_not have_title('| Contact') }
   end
 
 
